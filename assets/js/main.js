8f6d3eb34b38bf3f69305aae1723ef995731e811
Matt Caswell <matt.caswell@hotmail.com>
2021-10-29 16:23:15 +0200

    form design and basic js complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Page chargée");
+
   document.getElementById("connect-button").addEventListener("click", () => {
     console.log("The button was clicked!!");
     document.getElementById("modal").style.display = "flex"
+  });
+
+  document.getElementById("close-form").addEventListener("click", () => {
+    console.log("The button was clicked!!");
+    document.getElementById("modal").style.display = "none"
   });
   
 });
