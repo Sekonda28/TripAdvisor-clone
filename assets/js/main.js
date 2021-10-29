30275aed55051c1d7755e8b5295788e138f27336
Matt Caswell <matt.caswell@hotmail.com>
2021-10-29 15:45:58 +0200

    add js inside Dom content loaded

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Page chargée");
-});
-
-document.getElementById("connect-button").addEventListener("click", () => {
-  console.log("The button was clicked!!");
-  document.getElementById("modal").style.display = "flex"
+  document.getElementById("connect-button").addEventListener("click", () => {
+    console.log("The button was clicked!!");
+    document.getElementById("modal").style.display = "flex"
+  });
+  
 });
